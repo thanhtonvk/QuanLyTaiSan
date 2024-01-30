8c670a54af2fd03e382e097c745a0a1d1be31d50
tondt <tondt@dyno.vn>
2024-01-30 22:46:04 +0700

    add

--- UI/ControlUI/CtrlQuanLyTaiSan.cs
+++ UI/ControlUI/CtrlQuanLyTaiSan.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyVatTu.DAO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,44 +8,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QuanLyVatTu.Models;
 
 namespace QuanLyVatTu.UI.ControlUI
 {
     public partial class CtrlQuanLyTaiSan : UserControl
     {
+        private TaiSanDAO _taiSanDao = new TaiSanDAO();
+        private NganhDAO _nganhDao = new NganhDAO();
+        private LoaiTaiSanDAO _loaiTaiSanDao = new LoaiTaiSanDAO();
+        private List<Nganh> dsNganhs;
+        private List<LoaiTaiSan> dsLoaiTS;
+        private List<string> dsTenNganhs;
+        private List<string> dsTenLoaiTSs;
+
         public CtrlQuanLyTaiSan()
         {
             InitializeComponent();
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            loadDs("");
+        }
+
+        private void loadDs(string tuKhoa)
+        {
+            var list = _taiSanDao.DsTaiSan(tuKhoa);
+            dataGridView1.DataSource = list;
+            dsNganhs = _nganhDao.DsNganh("");
+            dsLoaiTS = _loaiTaiSanDao.DsLoaiTaiSan("");
+
+         
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
         {
-
         }
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-
         }
     }
 }
